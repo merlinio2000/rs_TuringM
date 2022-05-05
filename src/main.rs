310c2a70e23637371b69c2a9e02282d835b1eaf2
merlinio2000 <merlin@merlinio2000>
2022-05-05 20:57:12 +0200

    Working Emulator (TM itself still needs some work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use lazy_regex::*;
 use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::fmt;
 
 #[derive(Debug)]
 enum MoveDir {
@@ -29,6 +30,16 @@ impl From<&str> for MoveDir {
     }
 }
 
+impl fmt::Display for MoveDir {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", match self {
+            MoveDir::LEFT => "L",
+            MoveDir::RIGHT => "R"
+        }
+        )
+    }
+}
+
 #[derive(Debug)]
 enum State {
     Q(usize),
@@ -50,6 +61,9 @@ type TransitionKey = (QState, Symbol);
 type TransitionStep = (QState, Symbol, MoveDir);
 type TransitionTuple = (TransitionKey, TransitionStep);
 
+pub const EMPTY_WORD: Symbol = 3;
+pub const TRANS_SEPARATOR: &str = "11";
+
 #[derive(Debug)]
 struct TuringM<'a> {
     tape: VecDeque<Symbol>,
@@ -62,24 +76,36 @@ impl TuringM<'_> {
     fn step(&mut self) -> bool {
         match self.transitions.get(&(self.state, self.tape[self.ptr])) {
             Some(transition) => {
-                self.state = transition.1;
+                println!("Pointer: {} ({}, {}) -> {:?}", self.ptr, self.state, self.tape[self.ptr], transition);
+                println!("Tape: {:?}", self.tape);
+                println!();
+                self.state = transition.0;
                 self.tape[self.ptr] = transition.1;
                 let new_idx = self.ptr as i32 + transition.2.tape_ptr_move();
                 if new_idx < 0 {
-                    self.tape.push_front(3);
+                    println!("prepending tape");
+                    self.tape.push_front(EMPTY_WORD);
                     // the index of our element is now off by one because we prepended
-                    self.ptr += 1;
+                    self.ptr = (new_idx + 1) as usize;
                 } else if new_idx == self.tape.len() as i32 {
-                    self.tape.push_back(3);
+                    println!("appending tape");
+                    self.tape.push_back(EMPTY_WORD);
+                    self.ptr = new_idx as usize;
                 } else if new_idx > self.tape.len() as i32 {
                     panic!("Machine tape overflow!");
+                } else {
+                    self.ptr = new_idx as usize;
                 }
                 true
             }
-            None => false
+            None => {
+                println!("Coulnt find transition for ({}, {})", self.state, self.tape[self.ptr]);
+                false
+            }
         }
     }
 }
+
 
 fn parse_transition(trans_str: &str) -> TransitionTuple {
     if regex_is_match!(r"(0+)1(0+)1(0+)1(0+)1(0{1,2})", trans_str) {
@@ -87,12 +113,16 @@ fn parse_transition(trans_str: &str) -> TransitionTuple {
             regex_captures!(r"(0+)1(0+)1(0+)1(0+)1(0{1,2})", trans_str).unwrap();
         // symbol is its len(== number) - 1
         (
-            (l_q_from.len(), l_symbol_read.len() - 1),
-            (l_q_to.len(), l_symbol_wrote.len() - 1, l_move_dir.into()),
+            (l_q_from.len(), l_symbol_read.len()),
+            (l_q_to.len(), l_symbol_wrote.len(), l_move_dir.into()),
         )
     } else {
         panic!("{} doesnt represent a valid transition", trans_str)
     }
+}
+
+fn parse_tape(tape_str: &str) -> VecDeque<Symbol> {
+    tape_str.chars().map(|c| (c.to_digit(10).unwrap() as usize) + 1).collect()
 }
 
 fn main() {
@@ -104,9 +134,9 @@ fn main() {
     }
     let tm_str = &args[0];
     */
-    let TRANS_SEPARATOR = "11";
+    let tape_str = "0001000";
 
-    let tm_str = "0100100010100";
+    let tm_str = "010101000010011010000010100001001101001000100100110001001000100100110001010000100101100001001000010010110000010000100000100001011000010000100000100000101100000101000001010110000010001000000101001100000010000100000010000100110000001000001000010000010110000001010000001010011000010100001010011000000010010000000100010110000000100001000000010001011000000010100101001100010001000100010";
 
     //let transitions: Vec<Transition> = tm_str.split(TRANS_SEPARATOR).map(|trans_str| trans_str.into()).collect();
 
@@ -116,13 +146,19 @@ fn main() {
         .collect();
 
     let mut machine = TuringM {
-        tape: VecDeque::new(),
+        tape: parse_tape(tape_str),
         ptr: 0,
         state: 1,
         transitions: &transition_map
     };
 
+
     println!("Turin world");
 
     println!("{:?}", transition_map);
+
+
+    while machine.step() {}
+    println!("Machine ended in State {}", machine.state);
+    println!("Tape: {:?}", machine.tape);
 }
